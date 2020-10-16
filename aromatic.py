b35015d60ef97f127207d78d339c53475d8c14bb
BahrMikhev <BahrMikhev@users.noreply.github.com>
2020-10-16 10:31:54 +0400

    atom_info.py added typo fixed

--- aromatic.py
+++ aromatic.py
@@ -1,3 +1,7 @@
+from struct import Struct, Atom
+import atom_info as AtomInfo
+
+
 class Aromatic:
     smilesStr = ""
 
@@ -5,9 +9,24 @@ class Aromatic:
         self.smilesStr = smilesStr
 
     def isAromatic(self) -> bool:
-        # Current smilesStr must be correct SMILES sequenсe
+        # Current smilesStr must be correct SMILES sequence
         for c in self.smilesStr:
             if c.isalpha() and c.isupper():
                 return False
         else:
             return True
+
+    def generate(self) -> Struct:
+
+        if not self.isAromatic():
+            return None
+
+        __num_of_carbon = 0  # Number of carbon atoms
+        __num_of_possible = 0  # Number of electron pairs, that cun be donored to congureated system
+        for c in self.smilesStr:
+            if not c.isalpha():
+                continue
+            if c == 'c':
+                __num_of_carbon += 1
+            if AtomInfo.getValentElctrons(Atom(c)) > 4:
+                __num_of_possible += (AtomInfo.getValentElctrons(Atom(c)) - 4)
--- struct.py
+++ struct.py
@@ -1,28 +1,32 @@
 class Atom:
     __type = ""
 
-    def __init__(self, type):
-        self.__type = type
+    def __init__(self, type_):
+        self.__type = type_
 
     def getType(self) -> str:
         return self.__type
 
 
 class Struct:
-    # This dict for all atoms has their nums
-    __data = {}
-    # This matrix for all pairs of atoms has th bond type value or 0 if their is no bond
-    __bondMatrix = {}
-    # Current num of atoms in struct
-    __numOfAtoms = 0
 
-    # This function add new atom and retrun his num
+    __MAX_NUM_OF_ATOMS = 100
+
+    __data = dict()  # This dict for all atoms has their nums
+    __bondMatrix = list()  # This matrix for all pairs of atoms has the bond type value or 0 if there is no bond
+    __numOfAtoms = 0  # Current num of atoms in struct
+
+    def __init__(self):
+        self.__bondMatrix = [list(0 for i in range(self.__MAX_NUM_OF_ATOMS)) \
+                             for i in range(self.__MAX_NUM_OF_ATOMS)]
+
+    # This function add new atom and return his num
     def addAtom(self, atom) -> int:
         self.__data[self.__numOfAtoms] = atom
         __numOfAtoms = self.__numOfAtoms + 1
         return __numOfAtoms - 1
 
-    # This function return Flase if bod was already made and True if atoms were just connected
+    # This function return False if bod was already made and True if atoms were just connected
     def addBond(self, num1, num2, bondType) -> bool:
         if self.__bondMatrix[num1][num2] != 0:
             return False
